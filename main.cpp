3bd2353eb047da230e17d55dc07d8b945fae7bfa
csharp14 <104353572+csharp14@users.noreply.github.com>
2023-03-05 10:07:21 +0000

    Began ray tracing code
    Started following the program detailed at https://raytracing.github.io/books/RayTracingInOneWeekend.html to practice/learn C++

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@ int main(){
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
     for (int j = image_height-1; j>=0; --j){
+        std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
         for (int i = 0; i < image_width; ++i){
         auto r = double(i) / (image_width-1);
         auto g = double(j) / (image_height-1);
@@ -18,7 +19,7 @@ int main(){
         int ig = static_cast<int>(255.999 * g);
         int ib = static_cast<int>(255.999 * b);
         
+        std::cout << ir << ' ' << ig << ' ' << ib << '\n';
         }
     }
-    return 0;
 }
