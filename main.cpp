044dbb210441585e963ae022dbd186ff3080804f
csharp14 <104353572+csharp14@users.noreply.github.com>
2023-11-17 20:33:41 +0000

    Completed through until Section 6.7
    Followed Ray Tracing In One Weekend guide through Section 6.7.

--- main.cpp
+++ main.cpp
@@ -1,23 +1,17 @@
+#include "rtweekend.h"
 #include "color.h"
-#include "vec3.h"
-#include "ray.h"
+#include "hittable.h"
+#include "hittable_list.h"
+#include "sphere.h"
+
 #include <iostream>
 
-bool hit_sphere(const point3& center, double radius, const ray& r) {
-    vec3 oc = r.origin() - center;
-    auto a = dot(r.direction(), r.direction());
-    auto b = 2.0 * dot(oc, r.direction());
-    auto c = dot(oc, oc) - radius*radius;
-    auto discriminant = b*b - 4*a*c;
-    return (discriminant >= 0);
-}
+color ray_color(const ray& r, const hittable& world) {
+    hit_record rec;
+    if (world.hit(r,0,infinity,rec)) {
+        return 0.5 * (rec.normal + color(1,1,1));
+    }
 
-color ray_color(const ray& r) {
-    //return color(0,0,0);
-
-    if (hit_sphere(point3(0,0,-1), 0.5, r))
-        return color(1,0,0);
-        
     vec3 unit_direction = unit_vector(r.direction());
     auto a = 0.5*(unit_direction.y() + 1.0);
     return (1.0-a)*color(1.0,1.0,1.0) + a*color(0.5,0.7,1.0);
@@ -27,19 +21,22 @@ int main(){
     // Image size
     auto aspect_ratio = 16.0/9.0;
     int image_width = 400;
-    //int image_height = 256;
 
     // Calculate the image height and ensue that it's at least 1
     int image_height = static_cast<int>(image_width / aspect_ratio);
     image_height = (image_height < 1) ? 1 : image_height;
 
-    // Viewport widths less than 1 are ok since they are real valued.
-    auto viewport_height = 2.0;
-    auto viewport_width = viewport_height * (static_cast<double>(image_width)/image_height);
+    // World
+    hittable_list world;
 
+    world.add(make_shared<sphere>(point3(0,0,-1), 0.5));
+    world.add(make_shared<sphere>(point3(0,-100.5,-1), 100));
+  
     // Camera
     auto focal_length = 1.0;
     auto camera_center = point3(0,0,0);
+    auto viewport_height = 2.0;
+    auto viewport_width = viewport_height * (static_cast<double>(image_width)/image_height);
 
     // Calculate vectors across the horizontal and down the vertical viewport edges
     auto viewport_u = vec3(viewport_width, 0, 0);
@@ -51,8 +48,7 @@ int main(){
 
     // Calculate the location of the upper left pixel
     auto viewport_upper_left = camera_center
-                            - vec3(0,0,focal_length) 
-                            - viewport_u/2 - viewport_v/2; 
+                            - vec3(0,0,focal_length) - viewport_u/2 - viewport_v/2; 
     auto pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
     
     // Render
@@ -65,7 +61,7 @@ int main(){
             auto ray_direction = pixel_center - camera_center;
             ray r(camera_center, ray_direction);
 
-            color pixel_color = ray_color(r);
+            color pixel_color = ray_color(r, world);
             write_color(std::cout, pixel_color);
         }
     }
